06ef740ecc3a23665aab9d585c2834f4b929c9ee
xiaozhao <xz1001e_mail@163.com>
2018-07-20 22:48:34 +0800

    fix media store path

--- prot.h
+++ prot.h
@@ -46,12 +46,12 @@
 
 #if defined ENABLE_ADAS
 #define DO_DELETE_SNAP_SHOT_FILES "rm -r /data/snap/adas/*"
-#define SNAP_SHOT_JPEG_PATH "/data/snap/"
+#define SNAP_SHOT_JPEG_PATH "/data/snap/adas"
 //#define SNAP_SHOT_JPEG_PATH "/mnt/obb/adas/"
 
 #elif defined ENABLE_DMS
 #define DO_DELETE_SNAP_SHOT_FILES "rm -r /data/snap/dms/*"
-#define SNAP_SHOT_JPEG_PATH "/data/snap/"
+#define SNAP_SHOT_JPEG_PATH "/data/snap/dms/"
 //#define SNAP_SHOT_JPEG_PATH "/mnt/obb/dms/"
 #endif
 
